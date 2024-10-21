418ea79da138500e2c035e690cdbf87d1e992bcc
Arnab Das Utsa <144443522+iUtsa@users.noreply.github.com>
2024-10-21 23:28:12 +0000

    app.py updated

--- app.py
+++ app.py
@@ -10,15 +10,30 @@ db = mysql.connector.connect(
     database="Customer_and_Employee_and client_Information"
 )
 
+
 @app.route('/')
 def home():
     return render_template('client/index.html')
-#type code below do not adjust the information above
 
-#def create_account():
-   # if
+@app.route('/about')
+def about():
+    return render_template('client/about.html')
 
+@app.route('/services')
+def services():
+    return render_template('client/services.html')
 
+@app.route('/login')
+def login():
+    return render_template('client/login.html')
+
+@app.route('/nailcare')
+def nailcare():
+    return render_template('client/nailcare.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('client/contact.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
